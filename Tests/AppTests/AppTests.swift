5b5ed0a546245de458a48f8aa6601e1eaa9eace3
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-05-18 16:29:56 +0300

    Added new tests for get requests

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -4,15 +4,18 @@ import XCTVapor
 final class AppTests: XCTestCase {
     func testSafeShell() throws {
         
-        let listFiles: String = "book.md\nfile.md\nfiles.md\npage1.1.md\npage1.md\n"
-        
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "home", afterResponse: { res in
+        try app.test(.GET, "list", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            XCTAssertEqual(res.body.string, "book.md\nfile.md\nfiles.md\npage1.1.md\npage1.md\n")
+        })
+        
+        try app.test(.GET, "list/page1", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Saving data with FileManager is easy!")
         })
     }
 }
