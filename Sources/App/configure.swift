f559c394fdf57c92ed99491bc9bd76e782995807
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-05-18 16:34:15 +0300

    CORS Middleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,17 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
     // register routes
     try routes(app)
+    
+    
+    // cors middleware should come before default error middleware using `at: .beginning`
+    app.middleware.use(cors, at: .beginning)
 }
