c8561905fbd8809e035d1de782722505b1665496
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-05-25 17:00:06 +0300

    Added upload files function

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,7 +3,20 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // set max body size
+    app.routes.defaultMaxBodySize = "10mb"
+
+    app.post("uploadFile") { req -> EventLoopFuture<String> in
+        let key = try req.query.get(String.self, at: "key")
+        let path = "/Users/kl/Desktop/" + key
+        
+        return req.body.collect()
+            .unwrap(or: Abort(.noContent))
+            .flatMap { req.fileio.writeFile($0, at: path) }
+            .map { key }
+    }
 
     let corsConfiguration = CORSMiddleware.Configuration(
         allowedOrigin: .all,
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,30 +19,6 @@ func routes(_ app: Application) throws {
         return showContent
     }
     
-    app.post("uploadFile") { req -> EventLoopFuture<String> in
-        struct Input: Content {
-            var file: File
-        }
-        let input = try req.content.decode(Input.self)
-        
-        let path = app.directory.publicDirectory + input.file.filename
-        
-        return req.application.fileio.openFile(path: path,
-                                               mode: .write,
-                                               flags: .allowFileCreation(posixMode: 0x744),
-                                               eventLoop: req.eventLoop)
-            .flatMap { handle in
-                req.application.fileio.write(fileHandle: handle,
-                                             buffer: input.file.data,
-                                             eventLoop: req.eventLoop)
-                    .flatMapThrowing { _ in
-                        try handle.close()
-                        return input.file.filename
-                    }
-            }
-        
-    }
-    
     app.get("home") { req -> String in
         return "Hello, world!"
     }
