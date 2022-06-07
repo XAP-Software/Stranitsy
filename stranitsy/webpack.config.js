6382adf26b65cf7967870c659a0313aee248718f
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-06-07 16:31:15 +0300

    Updated

--- stranitsy/webpack.config.js
+++ stranitsy/webpack.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
 module.exports = {
@@ -11,7 +12,7 @@ module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"),
 
   output: {
-    filename: "main.js",
+    filename: "App.js",
     path: path.resolve(__dirname, "dist"),
   },
 
