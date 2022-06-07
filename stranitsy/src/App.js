49a8a61023d0848563f53d7e53ff2f86ec49a9cd
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-06-07 16:33:58 +0300

    A new axios handler has been added to get the data structure and content.

--- stranitsy/src/App.js
+++ stranitsy/src/App.js
@@ -1,46 +1,39 @@
 import './App.css';
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import Editor from "rich-markdown-editor";
-// import Sidebar from "./sidebar";
-import ListPages from './components/ListPages';
-import multiNavbar from './components/multiNavBar';
-import { Router, Route } from 'react-router-dom';
-import Page from './components/Page';
+import { StyledEditor } from './components/editor';
 import { debounce } from "lodash";
 import axios from 'axios';
+import { parse } from 'jekyll-markdown-parser';
 
-const element = document.getElementById("root");
-const main = ReactDOM.createRoot(document.getElementById('root'));
 const savedText = localStorage.getItem("saved");
 
+// const exampleText = `
+// # Hello Advocate Pod
 
-const exampleText = `
-# Hello Advocate Pod
+// ## H2
 
-## H2
+// ### H3
 
-### H3
+// #### H4
 
-#### H4
+// This is a markdown paragraph.
 
-This is a markdown paragraph.
+// - unordered lists
+// - lists
+// [this is a link](google.com)
+// 1. numbered
+// 2. lists
 
-- unordered lists
-- lists
-[this is a link](google.com)
-1. numbered
-2. lists
+// \`\`\`
+// const foo = 'foo'; // code
+// \`\`\`
 
-\`\`\`
-const foo = 'foo'; // code
-\`\`\`
+// ---
 
----
+// `;
 
-`;
-
-const defaultValue = savedText || exampleText;
+// const defaultValue = savedText || exampleText;
 
 var FileSaver = require('file-saver');
 
@@ -55,7 +48,31 @@ class App extends React.Component {
     readOnly: false,
     dark: localStorage.getItem("dark") === "enabled",
     value: undefined,
+    content: '',
+    pages: [],
+
   };
+
+  componentDidMount () {
+    axios.get(`http://localhost:8080/list`)
+      .then(res => {
+        const pages = res.data;
+        this.setState({ pages })
+      });
+    
+    console.log(this.state.pages)
+  }
+
+  showContent (page) {
+    axios.get(`http://localhost:8080/list${page}`)
+      .then(res => {
+        // if (res.data === '') console.log('empty string')
+        var content;
+        if (res.data !== '') content = res.data;
+        else content = '';
+        this.setState({ content })
+      });
+  }
 
   handleToggleReadOnly = () => {
     this.setState({ readOnly: !this.state.readOnly });
@@ -73,10 +90,9 @@ class App extends React.Component {
     localStorage.setItem("saved", value);
     
     this.setState({ value });
-    console.log(value)
   };
 
-  handleChange = debounce(value => {
+  handleChanges = debounce(value => {
     const text = value();
     localStorage.setItem("saved", text);
   }, 250);
@@ -97,47 +113,65 @@ class App extends React.Component {
 
   
   handleChange = (event) => {
-    this.setState.File(event.target.files[0])
+    this.setState({file: event.target.files[0]})
+    console.log(this.setState({file: event.target.files[0]}))
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const url = 'http://localhost:8080/uploadFile';
+  onFileUpload = (page) => {
+    
     const formData = new FormData();
-    formData.append('file', this.state.file);
-    formData.append('fileName', this.state.file.name);
-    console.log(formData)
-    const config = {
-      headers: {
-        'content-type': 'text/plain',
-      },
-    };
-    axios.post(url, formData, config).then((response) => {
-      console.log(response.data);
-    })
-  }
-
   
+    formData.append(
+      "file",
+      this.state.file,
+      this.state.file.name
+    );
+  
+    console.log(this.state.file);
+
+    axios.post(`http://localhost:8080/updatedFile?key=${page}`, formData);
+  };
+
   render() {
     const { body } = document;
     if (body) body.style.backgroundColor = this.state.dark ? "#181A1B" : "#FFF";
+    console.log(this.state.content)
     
     return (
-      <React.Fragment>
+      <div>
+        <StyledEditor
+          dir={"auto"}
+          ref={ref => (this.element = ref)}
+        />
         <div className='App'>
-
-          <form onSubmit={this.handleSubmit}>
-            <input type="file" onChange={this.handleChange}/>
-          </form>
-
           <div className='sidebarSpace'>
-            <ListPages/>
+            <ul>
+              {
+                this.state.pages?.map(page => 
+                    <li key={page.name}>
+                      <button className='ul-button' onClick={() => {this.showContent(page.url)}}>
+                        {/* <a href={page.url}>{page.name}</a> */}{page.name}
+                      </button>
+                    </li>
+                  )
+              }
+            </ul>
           </div>
           
           <div className='editorSpace'>
             <div>
               <br />
-              <button type="button" onClick={this.saveFile}>Save file</button>
+              <div>
+                <div>
+                  <input type="file" onChange={this.handleChange} />
+                  {this.state.pages?.map(page => 
+                    <button key={page.name} onClick={() => {this.onFileUpload(page.name)}}>
+                      {page.name}
+                    </button>
+                  )}
+                </div>
+              </div>
+              {/* <button type="button" onClick={this.saveFile}>Save file</button> */}
 
               <button type="button" onClick={this.handleToggleReadOnly}>
                 {this.state.readOnly ? "Editable" : "Read only"}
@@ -145,20 +179,20 @@ class App extends React.Component {
               <button type="button" onClick={this.handleToggleDark}>
                 {this.state.dark ? "Light theme" : "Dark theme"}
               </button>{" "}
-              <button type="button" onClick={this.handleSaveValue}>
+              {/* <button type="button" onClick={this.handleSaveValue}>
                 Update value
-              </button>
+              </button> */}
             </div>
             <br />
             <br />
             <Editor
-              id="example"
+              id="examples"
               readOnly={this.state.readOnly}
-              value={this.state.value}
-              defaultValue={defaultValue}
+              value={this.state.content}
+              placeholder="Document content goes here..."
               onSave={options => console.log("Save triggered", options)}
               onCancel={() => console.log("Cancel triggered")}
-              onChange={this.handleChange}
+              onChange={this.handleChanges}
               onClickLink={href => console.log("Clicked link: ", href)}
               onClickHashtag={tag => console.log("Clicked hashtag: ", tag)}
               onCreateLink={title => {
@@ -195,13 +229,9 @@ class App extends React.Component {
             />
           </div>
         </div>
-    </React.Fragment>
+        </div>
     );
   }
-}
-
-if (element) {
-  ReactDOM.render(<App />, element);
 }
 
 export default App;
