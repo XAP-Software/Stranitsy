9eafbc017bbfe173cf09c4e33dd51d9187105c91
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-07-20 16:47:04 +0300

    The first version of the launch simultaneously vapor with react

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -3,16 +3,40 @@ import Vapor
 import Foundation
 import Dispatch
 
-#if DEBUG 
-print("This never gets printed")
-#else
-print("this always executes instead")
-#endif
+#if DEBUG
 
+print("This never gets printed")
+
+func asyncRun () async throws {
+    let app = Application()
+    async let shellController = ShellController()
+
+    try configure(app)
+    let _ = try await shellController.unixCommand("cd `pwd`/Client/", path: "&& yarn start")
+    try app.run()
+    app.shutdown()
+    print("before")
+}
+
+func executeAsyncRun () {
+    Task.detached {
+        try await asyncRun()
+    }
+}
+
+executeAsyncRun ()
+
+#else
+
+print("this always executes instead")
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
 try app.run()
+dispatchMain()
+
+#endif
+
 dispatchMain()
